108c854a1a08acf9d6ec75465b3ea4ddbcb8f120
Victor Calebe <victorcalebesilva10@gmail.com>
2021-10-14 17:00:26 -0300

    Finalizando funcao de atualização e inserção de historico

--- script/tela_jogo.js
+++ script/tela_jogo.js
@@ -15,6 +15,10 @@ function iniciar(){
     finalizar();
     
     cron = setInterval(() => { cronometro(); }, tempo);
+
+    //chamando a funcao apenas para testar o metodo de historico
+    atualizarHistorico("victor","10x10","8","dificil","20min","perdeu")
+    atualizarHistorico("calebe","5x5","0","facil","50min","ganhou")
 }
 
 //no momento em que o jogador vencer ou for derrotado
@@ -56,34 +60,74 @@ var tabelaHistorico = document.getElementById('tabelaHistorico');
 var listaHistoricos = [];
 
 function atualizarHistorico(jogador,dimensao,numBombas,modalidade,tempoGasto,resultado){
-    console.log("att");
-    /*if(listaHistoricos.length === 0){
-        tabelaHistorico.innerHTML = "<tr> <td class='historico'> Jogador: <br> Dimensão: <br> Número de Bombas:<br> Modalidade:<br> Tempo Gasto:<br> Resultado:<br> Data/Hora: </td> </tr>";
+
+    var today = new Date()
+    var GMTstring = today.toGMTString()
+
+    var templateHistorico = {
+        t_jogador: jogador,
+        t_dimensao: dimensao,
+        t_numBombas: numBombas,
+        t_modalidade: modalidade,
+        t_tempoGasto: tempoGasto,
+        t_resultado: resultado,
+        t_data: GMTstring,
+    };
+
+    listaHistoricos.push(templateHistorico);
+
+    /*if(templateHistorico.t_jogador == undefined){
+        var vazio = document.createElement('h3')
+        vazio.innerHTML = "Sem historico"
+        tabelaHistorico.appendChild(vazio)
         return;
     }*/
-    var quebraLinha = document.createElement('br')
-    var linha = document.createElement('tr')
-    linha.className='historico'
     
+    var historico = listaHistoricos[(listaHistoricos.length - 1)];
+    //alert(historico.t_data);
 
-    var campoJogador = document.createElement('td')
-    campoJogador.innerText = "Jogador: " + jogador;
+    var linha = document.createElement('ul')
+    var campoJogador = document.createElement('li')
+    var campoDimensao = document.createElement('li')
+    var campoNumBombas = document.createElement('li')
+    var campoModalidade = document.createElement('li')
+    var campoTempoGasto = document.createElement('li')
+    var campoResultado = document.createElement('li')
+    var campoData = document.createElement('li')
 
-    var campoDimensao = document.createElement('td')
-    campoDimensao.innerText = "Dimensao: " + dimensao;
-
+    linha.className='historico'
     campoJogador.className='historico'
     campoDimensao.className='historico'
-    
-    //dados.innerHTML = new Date();
+    campoNumBombas.className='historico'
+    campoModalidade.className='historico'
+    campoTempoGasto.className='historico'
+    campoResultado.className='historico'
+    campoData.className='historico' 
+
+    campoJogador.innerText = "Jogador: " + historico.t_jogador;
+    campoDimensao.innerText = "Dimensao: " + historico.t_dimensao;
+    campoNumBombas.innerText = "Numero de Bombas: " + historico.t_numBombas;
+    campoModalidade.innerText = "Modalidade: " + historico.t_modalidade;
+    campoTempoGasto.innerText = "Tempo levado: " + historico.t_tempoGasto;
+    campoResultado.innerText = "Resultado: " + historico.t_resultado;
+    campoData.innerText = historico.t_data;
+
     linha.appendChild(campoJogador)
-    linha.appendChild(quebraLinha)
     linha.appendChild(campoDimensao)
+    linha.appendChild(campoNumBombas)
+    linha.appendChild(campoModalidade)
+    linha.appendChild(campoTempoGasto)
+    linha.appendChild(campoResultado)
+    linha.appendChild(campoData)
+
     tabelaHistorico.appendChild(linha)
+
+    return(listaHistoricos)
+
 }
 
 
 
 //evento ao carregar a pag
-
-window.addEventListener('load',atualizarHistorico("victor","10x10","8","dificil","20min","perdeu"));
+//window.addEventListener('load',atualizarHistorico("victor","10x10","8","dificil","20min","perdeu"));
+//window.addEventListener('load',atualizarHistorico("calebe","5x5","0","facil","50min","ganhou"));
